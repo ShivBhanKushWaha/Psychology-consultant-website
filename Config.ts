28655f376f9ff041d101265c700aedf7aa283b23
Shivbhan Kushwaha <108090221+ShivBhanKushWaha@users.noreply.github.com>
2024-07-29 19:06:04 +0530

    Update Config.ts

--- Config.ts
+++ Config.ts
@@ -1,2 +1,2 @@
-export const SERVER_BASE_URL='https://major-project-server-170m.onrender.com'
+export const SERVER_BASE_URL='https://major-project-server-xpwx.onrender.com'
 // export const SERVER_BASE_URL='http://localhost:5000'
